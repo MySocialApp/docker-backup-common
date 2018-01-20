a72cdea5789f42c6d6391c4b0119d3874d320b69
Pierre Mavro <pmavro@4tech.io>
2018-01-20 16:05:51 +0100

    feat: supporting debian in addition of alpine

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 # Install common tools and packages
-apk update && apk add curl duplicity openssl bash && \
-rm -rf /tmp/* /var/tmp/* /var/cache/apk/* /var/cache/distfiles/*
+if [ -f /etc/alpine-release ] ; then
+  apk update && apk add curl duplicity openssl bash && \
+  rm -rf /tmp/* /var/tmp/* /var/cache/apk/* /var/cache/distfiles/*
+elif [ -f /etc/debian_version ] ; then
+  apt-get update && apt-get -y install curl duplicity python-boto openssl && \
+  apt-get remove --purge && rm -Rf /var/lib/apt/lists/*
+fi
 
 # Get kubectl
 curl -o /usr/bin/kubectl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
