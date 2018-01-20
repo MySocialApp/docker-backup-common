57cbe8443f1dca1accaf3ca33a355955b1a36e4f
Pierre Mavro <pmavro@4tech.io>
2018-01-20 11:00:09 +0100

    refactor: download latest kubectl version

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ apk update && apk add curl duplicity openssl bash && \
 rm -rf /tmp/* /var/tmp/* /var/cache/apk/* /var/cache/distfiles/*
 
 # Get kubectl
-curl -o /usr/bin/kubectl -LO https://storage.googleapis.com/kubernetes-release/release/v1.8.6/bin/linux/amd64/kubectl && \
+curl -o /usr/bin/kubectl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
 chmod +x /usr/bin/kubectl
